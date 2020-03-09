3859d23af4d99ec85edf60ea3aade77d440c8060
B00104455 Cian O Cleirigh <B00104455@student.itb.ie>
2020-03-09 14:35:30 +0000

    adding tennisClub app

---
+++ resources/views/layouts/navAuth.blade.php
@@ -0,0 +1,5 @@
+<ul class="nav navbar-nav pull-right"> 
+    <li><a href="#register">Register<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a></li> 
+    <li><a href="#Login">Login<span class="glyphicon glyphicon-log-in" aria-hidden="true"></span></a></li> 
+    <li><a href="#Logout">Logout<span class="glyphicon glyphicon-log-out" aria-hidden="true"></span></a></li> 
+</ul>
---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::get('/home', 'HomeController@index')->middleware('verified');
+
+Route::resource('members', 'memberController');
+
+Route::get('/customers/new', 'CustomerController@new');
+
+Route::post('/customers/create', 'CustomerController@create')->name('customers.create');
+
+Route::get('/customers/edit/{id}', 'CustomerController@edit');
+
+Route::post('/customers/update', 'CustomerController@update');
+
+Route::resource('courts', 'courtController');
+
+Route::resource('bookings', 'bookingController');
+
+Route::get('/calendar/json','CalendarController@json')->name('calendar.json');
+
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+
+Route::get('/home','memberController@index')->name('home');
+
+Auth::routes();
+
